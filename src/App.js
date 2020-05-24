321e1d9a6cfc52641c81e67ef6538c4344e00871
sfukagawa <johannes.violoncello@gmail.com>
2020-05-24 08:00:25 +0000

    props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <div><h1>Hello, World!</h1></div>
-  }
+const App = () => {
+  const profiles = [
+    { name: "taro", age: 10 },
+    { name: "hanako", age: 5 },
+    { name: "Noname"},
+  ]
+  return (
+    <div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index}/>
+      })}
+    </div>
+  )
 }
 
+const User = props => {
+return <div>Hi, I am {props.name}, and {props.age} years old.</div>
+}
+
+User.defaultProps = {
+  age: 1
+}
 
 
 export default App;
